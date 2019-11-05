8f327c4eff7ec2c2fc2bbc1c4e81598d1801cba0
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-11-05 20:02:24 +0530

    header code alignment added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,38 +2,39 @@ import React from 'react';
 import { Nav, Navbar, NavDropdown, Form, FormControl } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-const Header = (props) => {
-    return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand>Movie Catalog</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Form className='w-75' onSubmit={props.onSubmitHandle} >
-                    <FormControl
-                        value={props.searchTerm}
-                        onChange={props.searchOnChange}
-                        type="text"
-                        placeholder="Search movie"
-                        className="mr-sm-2" />
-                </Form>
-                <Nav className="mr-auto">
-                    <NavDropdown title="Vigneshwaran" id="basic-nav-dropdown">
-                        <NavDropdown.Item>Logout</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    )
-}
+const Header = props => {
+	return (
+		<Navbar bg="light" expand="lg">
+			<Navbar.Brand>Movie Catalog</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Form className="w-75" onSubmit={props.onSubmitHandle}>
+					<FormControl
+						value={props.searchTerm}
+						onChange={props.searchOnChange}
+						type="text"
+						placeholder="Search movie"
+						className="mr-sm-2"
+					/>
+				</Form>
+				<Nav className="mr-auto">
+					<NavDropdown title="Vigneshwaran" id="basic-nav-dropdown">
+						<NavDropdown.Item>Logout</NavDropdown.Item>
+					</NavDropdown>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
+};
 
 Header.defaultProps = {
-    searchTerm: 'batman',
+	searchTerm: 'batman'
 };
 
 Header.propTypes = {
-    searchTerm: PropTypes.string,
-    onSubmitHandle: PropTypes.func,
-    searchOnChange: PropTypes.func
+	searchTerm: PropTypes.string,
+	onSubmitHandle: PropTypes.func,
+	searchOnChange: PropTypes.func
 };
 
 export default Header;
